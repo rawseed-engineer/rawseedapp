0aceac3fb8593964f3a1ac1fdc29a24ba3c3716f
Anthony Ng <engineer.anthony.ng@gmail.com>
2025-11-19 20:29:30 +0000

    Fix linting

--- src/ui/nav/footer.tsx
+++ src/ui/nav/footer.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import RawSeedLogo from "../../assets/rawseed_logo.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
