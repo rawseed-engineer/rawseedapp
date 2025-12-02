d0d5ba29e9b32ef3eaf6ee1a0d28d4eb23e372dc
Anthony Ng <rawseed.engineer@gmail.com>
2025-12-02 21:47:58 +0000

    Update contact us form

--- src/ui/home/joinOurJourney.tsx
+++ src/ui/home/joinOurJourney.tsx
@@ -4,8 +4,8 @@ import { useTranslation } from "react-i18next";
 const JoinOurJourney: React.FC = () => {
   const { t } = useTranslation();
   return (
-    <div className="max-w-md space-y-4">
-      <h1 className="text-balance text-5xl text-[#a18458] font-bold text-shadow-lg tracking-tight md:text-5xl">
+    <div className="max-w-md space-y-4 text-justify">
+      <h1 className="text-5xl text-[#a18458] font-bold text-shadow-lg tracking-tight md:text-5xl">
         {t("journey.title")}
       </h1>
       <p className="text-pretty text-neutral-600 text-2xl">{t("journey.p1")}</p>
